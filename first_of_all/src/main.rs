364098bb3edfcbd0570599d991504bcc3ba2ff91
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 03:09:44 +0900

    3.8

--- first_of_all/src/main.rs
+++ first_of_all/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    // println!("Hello, world!");
-    /*print!("Hello, ");
-    print!("Tokyo!");*/
-    println!("Hello, {}", "dogs!");
+    let a = 10;
+    // let 1a = 10;
+    // let !a = 10;
+    println!("{a}");
+
+    let mut b = 100;
+    b = 200;
+    
+    let c: i32 = 40;
+
+    let d;
+    d = 50;
+    
+    let d: &str = "test";
 }
