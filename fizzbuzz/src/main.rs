af0269902d2239b7176e2ff088a7d57d38255687
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 08:14:53 +0900

    5.24

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -14,10 +14,21 @@ fn fizzbuzz_while_if(n: u32) {
     }
 }
 
-fn fizzbuzz_for_match(n: u32) {}
+fn fizzbuzz_for_match(n: u32) {
+    for i in 1..=n {
+        match i % 15 {
+            0 => println!("fizzbuzz"),
+            3 | 6 | 9 | 12 => println!("fizz"),
+            5 | 10 => println!("buzz"),
+            _ => println!("{i}"),
+            // m => println!("{m}"),
+        }
+    }
+}
 
 fn fizzbuzz_for_match_tuple(n: u32) {}
 
 fn main() {
-    fizzbuzz_while_if(100);
+    // fizzbuzz_while_if(100);
+    fizzbuzz_for_match(100);
 }
