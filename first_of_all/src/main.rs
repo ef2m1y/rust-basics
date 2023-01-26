1287db2307f96a05f2c61394a08654df9ea3d41e
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 05:16:09 +0900

    3.15

--- first_of_all/src/main.rs
+++ first_of_all/src/main.rs
@@ -1,21 +1,16 @@
-use std::fmt::format;
+fn say_foo() {
+    println!("foo!!!");
+}
+
+fn mul(a: i32, b: i32) ->i32 {
+    a * b
+}
 
 fn main() {
-    let c1: char = 'a';
-    let c2: char = '@';
-    let c3: char = '😊';
+    say_foo();
+    println!("{}", mul(9, 9));
 
-    let s1: &str = "Rust";
-
-    let s2: String = String::from("Python");
-    let s3: String = "Go".to_string();
-
-    let mut s4: String = String::from("Hello");
-    s4.push_str(", Rust!");
-    println!("{}", s4);
-
-    // String + &str の順番でないとcompile error
-    println!("{}", s4 + "Zig!");
-
-    let s5: String = format!("{}{}", s1, s2);
+    let x: i32 = mul(3, 5);
+    
+    let u: () = say_foo();
 }
