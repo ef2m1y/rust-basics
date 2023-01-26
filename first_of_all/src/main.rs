8a25e8ac00884493c99c96b4d6b56ce2164e13a5
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 03:57:58 +0900

    3.13

--- first_of_all/src/main.rs
+++ first_of_all/src/main.rs
@@ -1,13 +1,24 @@
 fn main() {
-    let arr1: [i32; 3] = [1, 2, 3];
-    let arr2: [i32; 1000] = [0; 1000];
-    println!("{:?}", arr1);
+    let v1: Vec<i32> = vec![10, 20, 30];
+    let v2: Vec<i32> = vec![1; 100];
 
-    println!("{}", arr1[0]);
-    
-    let [x, y, z] = arr1;
-    println!("{x}");
+    let mut v3 = Vec::new();
+    v3.push(1);
+    v3.push(2);
+    v3.push(3);
+    println!("{:?}", v3);
 
-    let arr3: &[i32] = &arr1[..=1];
-    println!("{:?}", arr3);
+    let z: Option<i32> = v3.pop();
+    println!("{:?}", z);
+    println!("{:?}", v3);
+
+    let x: i32 = v3[0];
+    let y: Option<&i32> = v3.get(1);
+    println!("{}", x);
+    println!("{:?}", y); // Some(2)
+
+    let n: Option<&i32> = v3.get(1000);
+    println!("{:?}", n); // None
+
+    let s: &[i32] = &v3[..];
 }
