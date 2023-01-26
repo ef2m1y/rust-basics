dca527d0f6aea0860c36031917471364ab60b8a6
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 03:28:33 +0900

    3.10

--- first_of_all/src/main.rs
+++ first_of_all/src/main.rs
@@ -1,7 +1,12 @@
-const XYZ: i32 = 777;
-// let num = 1;
-
 fn main() {
-    const ABC: i32 = 100;
-    println!("{XYZ}");
+    // default type
+    let a: i32 = 1;
+    let b: f64 = 1.0;
+
+    let c = 3.0f32;
+    let d = 2 as f64 % b;
+
+    let e: bool = true;
+    let g: bool = 1 < 4;
+    let h: bool = 1 != 2;
 }
