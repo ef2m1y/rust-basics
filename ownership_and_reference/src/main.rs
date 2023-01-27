11c3c89f3871c98e9cb741a6e78880f704b55753
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 21:03:39 +0900

    6.30

--- ownership_and_reference/src/main.rs
+++ ownership_and_reference/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let x1: Vec<i32> = vec![1, 2, 3];
-    let x2: &Vec<i32> = &x1;
-    // let x3: &&Vec<i32> = &x2;
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let s = concat(&s1, &s2);
+    println!("{s}");
+    println!("{s1}");
+    println!("{s2}");
+}
 
-    // same
-    println!("{:?}", x1);
-    println!("x1 ptr: {:?}", x1.as_ptr());
-    println!("{:?}", x2);
-    println!("x2 ptr: {:?}", x2.as_ptr());
+fn concat(a: &String, b: &String) -> String {
+    let c = format!("{a}, {b}");
+    c
 }
