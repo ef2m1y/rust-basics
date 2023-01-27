716c28502b51634713eb5befcc50ff34238b5684
ef2m1y <shisotem01072002@gmail.com>
2023-01-27 12:48:03 +0900

    6.29

--- ownership_and_reference/src/main.rs
+++ ownership_and_reference/src/main.rs
@@ -1,24 +1,52 @@
+use std::fmt::format;
+
 fn main() {
-    let a = 100;
-    {
-        let mut v1 = vec![1, 2, 3];
-        println!("{:?}", v1);
+    let mut v1 = vec![1, 2, 3];
+    // (fat ptrに含まれる)v1へのptrはv1の0番目の要素のアドレスを指している
+    println!("v1 ptr: {:?}", v1.as_ptr());
+    println!("v1[0]: {:p}", &v1[0]);
 
-        let v2 = v1;
-        println!("{:?}", v2);
+    println!("v1 len: {}", v1.len());
+    println!("v1 capacity: {}", v1.capacity());
 
-        // v1.push(4);
-    }    
-    println!("{a}");
+    v1.push(4);
+    println!("v1 ptr: {:?}", v1.as_ptr());
+    println!("v1 len: {}", v1.len());
+    println!("v1 capacity: {}", v1.capacity());
+
+    // // move
+    // println!("v1 ptr: {:?}", v1.as_ptr());
+    // let v2 = v1;
+    // println!("v2 ptr: {:?}", v2.as_ptr());
+
+    let v2 = v1.clone();
+    println!("v1 ptr: {:?}", v1.as_ptr());
+    println!("v2 ptr: {:?}", v2.as_ptr());
+
+    let a = 10;
+    let b = a;
+    println!("a ptr: {:p}", &a);
+    println!("b ptr: {:p}", &b);
+
+    // 以降のタプルによる方法は冗長なので参照を用いることになる
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let (s, s1, s2) = concat(s1, s2);
+    println!("{s}");
+    println!("{s1}");
+    println!("{s2}");
 }
 
-// [ダングリングポインタ]------------------------------------------
-// 無効なメモリ領域を指すポインタはダングリングポインタと呼ばれる。
-// 特に, 本来有効だったメモリ領域が解放処理などによって無効化されたにも関わらず, 
-// そのメモリ領域を参照し続けているポインタのことをダングリングポインタと呼ぶ。
+fn concat(a: String, b: String) -> (String, String, String) {
+    let c = format!("{}, {}", a, b);
+    (c, a, b)
+}
 
-// 所有権のルール3(TRPL): "所有者(=変数)がスコープを外れたら, その所有者(=変数)が所有するメモリ上の値も破棄される"
-// -> つまり, "メモリの生存期間(=lifetime)が, そのメモリを所有している変数のスコープと同じ"
-// -> また, コンパイル時に所有権のチェックが行われる仕様である
-// -> コンパイル時に, (lifetimeを超えて, )解放済みメモリの参照が行われていないか判定可能である(-> compile error!)
-// ----------------------------------------------------------------
+// ・deep copy: .clone()で実現可能
+// ・shallow copy: moveにおいて元所有者の所有権も残存する形式(Rustにはない)
+//
+// ・moveの例外: Copyトレイト(スタックに保持される型に対して実装可)が実装された型はmoveでなくdeep copyが行われる
+//   - 値をcopyしても十分高速であると保証できる
+//
+// ・Copyトレイトが実装された型の例
+//   - 数値型, bool, char, タプル(要素全てCopyの場合)
