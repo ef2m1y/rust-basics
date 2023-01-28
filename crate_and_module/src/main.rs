466e1b2a71409108ed7e337ee3d4b518ef87e04e
ef2m1y <shisotem01072002@gmail.com>
2023-01-28 17:15:37 +0900

    8.40

--- crate_and_module/src/main.rs
+++ crate_and_module/src/main.rs
@@ -1,5 +1,29 @@
-use rand::Rng;
+mod test_module {
+    pub mod sub_module1 {
+        pub fn test_fn1() {
+            println!("call test_fn1-1");
+        }
+
+        fn test_fn2() {
+            println!("call test_fn1-2");
+        }
+    }
+    mod sub_module2 {
+        pub fn test_fn1() {
+            println!("call test_fn2-1");
+        }
+
+        fn test_fn2() {
+            println!("call test_fn2-2");
+        }
+    }
+}
+
+use test_module::sub_module1;
+
 fn main() {
-    let rand_num = rand::thread_rng().gen_range(1..=100);
-    println!("{rand_num}");
+    crate::test_module::sub_module1::test_fn1(); // 絶対パス
+    test_module::sub_module1::test_fn1(); // 相対パス(self::省略ver.)
+    // test_module::sub_module2::test_fn1();
+    sub_module1::test_fn1();
 }
