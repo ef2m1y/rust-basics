ecb8434aa0478bf65bd7eea22b327beb12d39404
ef2m1y <shisotem01072002@gmail.com>
2023-01-29 00:00:36 +0900

    9.51

--- trait_and_generics/src/main.rs
+++ trait_and_generics/src/main.rs
@@ -2,6 +2,8 @@
 // 型をimportしただけでは型に実装されたトレイトのメソッドは使用不可能
 // -> Shapeトレイトもimportする
 
+use std::fmt::Debug;
+
 fn main() {
     // let rect = Rectangle {
     //     width: 5.0,
@@ -24,25 +26,41 @@ fn main() {
     // println!("Rectangle double area: {}", double_area(&rect));
     // println!("Circle double area: {}", double_area(&circle));
 
-    println!("{:?}", (1, 2, 3));
-    // impl Debug for S よりも手軽
-    #[derive(Debug, PartialEq)]
-    struct S {
-        val1: i32,
-        val2: i32,
-    }
-    // the trait `Debug` is not implemented for `S`
-    println!("{:?}", S { val1: 1, val2: 2 });
 
-    let s1 = S {
-        val1: 1,
-        val2: 2,
-    };
-    let s2 = S {
-        val1: 1,
-        val2: 3,
-    };
-    // binary operation `==` cannot be applied to type `S`
-    // an implementation of `PartialEq<_>` might be missing for `S`
-    println!("{}", s1 == s2);
+    // println!("{:?}", (1, 2, 3));
+    // // impl Debug for S よりも手軽
+    // #[derive(Debug, PartialEq)]
+    // struct S {
+    //     val1: i32,
+    //     val2: i32,
+    // }
+    // // the trait `Debug` is not implemented for `S`
+    // println!("{:?}", S { val1: 1, val2: 2 });
+
+    // let s1 = S {
+    //     val1: 1,
+    //     val2: 2,
+    // };
+    // let s2 = S {
+    //     val1: 1,
+    //     val2: 3,
+    // };
+    // // binary operation `==` cannot be applied to type `S`
+    // // an implementation of `PartialEq<_>` might be missing for `S`
+    // println!("{}", s1 == s2);
+
+
+    println!("{}", max(1, 2));
+    println!("{}", max(1.1, 2.1));
+    println!("{}", max("a", "c"));
+}
+
+fn max<T>(a: T, b: T) -> T
+where T: PartialOrd + Debug
+{
+    if a >= b {
+        a
+    } else {
+        b
+    }
 }
