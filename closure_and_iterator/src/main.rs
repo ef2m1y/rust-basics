2c4ca3b2fdc1e7a2aacb655e675921fc50e22b29
ef2m1y <shisotem01072002@gmail.com>
2023-01-29 17:44:51 +0900

    11.59

--- closure_and_iterator/src/main.rs
+++ closure_and_iterator/src/main.rs
@@ -30,25 +30,36 @@ fn main() {
     // c3();
     // // println!("{:?}", v2); // err
 
-    let v = vec![1, 2, 3, 4, 5];
-    let v1_iter = v.iter();
-    for x in v1_iter {
-        println!("{}", x); // auto dereference
-    }
+    // let v = vec![1, 2, 3, 4, 5];
+    // let v1_iter = v.iter();
+    // for x in v1_iter {
+    //     println!("{}", x); // auto dereference
+    // }
 
-    // iteratorは内部に今が何番目かの情報を有する -> next毎に内部情報更新
-    // -> nextを呼び出すiterator(=v2_iter)はmutである必要がある
-    let mut v2_iter = v.iter();
-    // [v.iter() -> vの共有参照へのiterator] -> nextで得られる値は共有参照
-    println!("{:?}", v2_iter.next());
-    println!("{:?}", v2_iter.next());
-    println!("{:?}", v2_iter.next()); // "iteratorを消費する"
-    println!("{:?}", v2_iter.next());
-    println!("{:?}", v2_iter.next());
-    println!("{:?}", v2_iter.next()); // None
-    println!("{:?}", v2_iter.next()); // None
+    // // iteratorは内部に今が何番目かの情報を有する -> next毎に内部情報更新
+    // // -> nextを呼び出すiterator(=v2_iter)はmutである必要がある
+    // let mut v2_iter = v.iter();
+    // // [v.iter() -> vの共有参照へのiterator] -> nextで得られる値は共有参照
+    // println!("{:?}", v2_iter.next());
+    // println!("{:?}", v2_iter.next());
+    // println!("{:?}", v2_iter.next()); // "iteratorを消費する"
+    // println!("{:?}", v2_iter.next());
+    // println!("{:?}", v2_iter.next());
+    // println!("{:?}", v2_iter.next()); // None
+    // println!("{:?}", v2_iter.next()); // None
 
-    let mut v = vec![1, 2, 3, 4, 5];
-    let mut v2_iter = v.iter_mut();
-    // [v.iter_mut() -> vの可変参照へのiterator] -> nextで得られる値は可変参照
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let mut v2_iter = v.iter_mut();
+    // // [v.iter_mut() -> vの可変参照へのiterator] -> nextで得られる値は可変参照
 }
+
+// 全てのiteratorは次のようなiterator traitを実装している
+// pub trait Iterator {
+//     type Item; // 関連型: iteratorが扱う型を表す
+
+//     fn next(&mut self) -> Option<Self::Item>;
+
+//     // default method
+//     // default method
+//     // ...
+// }
