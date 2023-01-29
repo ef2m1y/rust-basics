4df09797c0c5e11f2c09b2aacf7683ff58c91be5
ef2m1y <shisotem01072002@gmail.com>
2023-01-29 12:10:00 +0900

    10.54

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
     // println!("{}", [1, 2, 3][10]); // panic
     // println!("{}", 1 / 0); // panic
-    panic!("This is my panic!");
+    // panic!("This is my panic!");
+
+    println!("{:?}", need_even(1));
+    println!("{:?}", need_even(2));
 }
+
+fn need_even(a: i32) -> Result<i32, String> {
+    if a % 2 == 0 {
+        Ok(a)
+    } else {
+        Err(String::from("I want an even number!"))
+    }
+}
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
