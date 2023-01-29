9e6177cc64b00fbdf37a05c7dbd2247b95d054f1
ef2m1y <shisotem01072002@gmail.com>
2023-01-29 12:15:01 +0900

    10.55

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -3,8 +3,19 @@ fn main() {
     // println!("{}", 1 / 0); // panic
     // panic!("This is my panic!");
 
-    println!("{:?}", need_even(1));
-    println!("{:?}", need_even(2));
+    // println!("{:?}", need_even(1));
+    // println!("{:?}", need_even(2));
+
+    let x = match need_even(2) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}", err);
+            panic!();
+        }
+    };
+    println!("{x}");
+
+    
 }
 
 fn need_even(a: i32) -> Result<i32, String> {
