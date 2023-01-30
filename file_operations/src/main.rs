72498e689ef849ebf013f7ea712717e94af4309f
ef2m1y <shisotem01072002@gmail.com>
2023-01-30 22:07:43 +0900

    14.74

--- file_operations/src/main.rs
+++ file_operations/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, io};
+use std::{env, fs, fs::File, io, io::prelude::*, io::BufReader};
 fn main() {
     // let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
@@ -6,11 +6,19 @@ fn main() {
     // // ["target/debug/file_operations", "aaa", "bbb", "ccc"]
     // // -> idxを指定し必要な値を取り出せる
 
-    print!("文字列を入力: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("入力された文字列: {:?}", input);
+    // print!("文字列を入力: ");
+    // let mut input = String::new();
+    // io::stdin().read_line(&mut input).unwrap();
+    // println!("入力された文字列: {:?}", input);
 
-    let mut num: i32 = input.trim().parse().unwrap();
-    println!("{:?}", num * 10);
+    // let mut num: i32 = input.trim().parse().unwrap();
+    // println!("{:?}", num * 10);
+
+    let mut f = File::open("src/sample1.txt").unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    println!("{}", contents);
+
+    let contests = fs::read_to_string("src/sample1.txt").unwrap();
+    println!("{}", contents);
 }
